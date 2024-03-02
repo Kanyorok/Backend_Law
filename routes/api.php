cffce280f7c20c260f96f7519aa06dd352b11aca
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2024-03-02 08:45:07 +0300

    Create the reservation route to use with the app

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CaseController;
+use App\Http\Controllers\ReservationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,3 +31,7 @@ Route::get('/cases', [CaseController::class, 'index']);
 Route::get('/cases/{id}', [CaseController::class, 'show']);
 Route::post('/cases', [CaseController::class, 'store']);
 Route::put('/cases/{id}', [CaseController::class, 'update']);
+
+Route::get('/reservations', [ReservationController::class, 'index']);
+Route::get('/reservations/{id}', [ReservationController::class, 'show']);
+Route::post('/reservations', [ReservationController::class, 'store']);
